0960d8a33aba541a1c531d50eb412e4d11dd2ab3
Russal Arya <russalarya@gmail.com>
2024-07-01 16:40:28 +1000

    Updated comments

--- app/src/views/SignIn.tsx
+++ app/src/views/SignIn.tsx
@@ -1,12 +1,16 @@
+import { FormEvent } from "react"
+
 const SignIn = () => {
   // write code here
 
+  const signIn = (event: FormEvent) => {
+    // write function data here
+  }
+
   // create output here
-  return (
-    <>
+  return (<form onSubmit={signIn}>
     <h1>Sign in</h1>
-    </>
-  )
+  </form>)
 }
 
 export default SignIn
