110bdfe48d2b7059af4c16b18d02447d55e63984
Russal Arya <russalarya@gmail.com>
2024-07-01 16:23:18 +1000

    Added basic UI

--- app/src/views/SignIn.tsx
+++ app/src/views/SignIn.tsx
@@ -1,11 +1,18 @@
 const SignIn = () => {
-  // write code here
+  let formInputs = [
+    {label: "Email", id: "email", type: "email"},
+    {label: "Password", id: "password", type: "password"}
+  ];
 
-  return (
-    <>
+  return (<>
     <h1>Sign in</h1>
-    </>
-  )
+    {formInputs.map(formInput => {
+      return (<div>
+        <label>{formInput.label}</label>
+        <input id={formInput.id} type={formInput.type}/>
+      </div>)
+    })}
+  </>)
 }
 
 export default SignIn
