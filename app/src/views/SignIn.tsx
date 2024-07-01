0a2a9b628a28b1e5264a9159d04fb97527632143
Russal Arya <russalarya@gmail.com>
2024-07-01 16:39:05 +1000

    Added sign in function

--- app/src/views/SignIn.tsx
+++ app/src/views/SignIn.tsx
@@ -1,10 +1,37 @@
+import { FormEvent } from "react";
+
 const SignIn = () => {
   let formInputs = [
     {label: "Email", id: "email", type: "email"},
     {label: "Password", id: "password", type: "password"}
   ];
 
-  return (<>
+  const signIn = (event: FormEvent) => {
+    event.preventDefault();
+
+    // @ts-ignore
+    const elementsArray = [...event.target.elements];
+
+    const data = elementsArray.reduce((acc, element) => {
+      if (element.id) {
+        acc[element.id] = element.value;
+      }
+
+      return acc;
+    }, {});
+
+    try {
+      if (data.email === '') throw("Please enter an email")
+      if (data.password === '') throw("Please enter a password")
+      if (data.password.length < 8) throw("Your password should be at least 8 characters long")
+    }
+    catch (error) {
+      alert(error);
+    }
+
+  }
+
+  return (<form onSubmit={signIn}>
     <h1>Sign in</h1>
     {formInputs.map(formInput => {
       return (<div>
@@ -12,7 +39,7 @@ const SignIn = () => {
         <input id={formInput.id} type={formInput.type}/>
       </div>)
     })}
-  </>)
+  </form>)
 }
 
 export default SignIn
