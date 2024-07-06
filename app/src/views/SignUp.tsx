fa52fd9285ce8b00d410409591ed702f480ba4a1
Russal Arya <russalarya@gmail.com>
2024-07-06 22:55:31 +1000

    Added sign up form elements

--- app/src/views/SignUp.tsx
+++ app/src/views/SignUp.tsx
@@ -3,8 +3,11 @@ import { FormInput, GenerateFormElements } from "./SignIn";
 
 const SignUp = () => {
   let formInputs: FormInput[] = [
+    {label: "First Name", id: "firstName", type: "text"},
+    {label: "Last Name", id: "lastName", type: "text"},
     {label: "Email", id: "email", type: "email"},
-    {label: "Password", id: "password", type: "password"}
+    {label: "Password", id: "password", type: "password"},
+    {label: "Confirm Password", id: "confirmPassword", type: "password"}
   ];
 
   const signUp = (event: FormEvent) => {
@@ -21,10 +24,15 @@ const SignUp = () => {
       return acc;
     }, {});
 
+    // error handling
     try {
+      if (data.firstName === '') throw("Please enter your first name")
+      if (data.lastName === '') throw("Please enter your last name")
       if (data.email === '') throw("Please enter an email")
       if (data.password === '') throw("Please enter a password")
       if (data.password.length < 8) throw("Your password should be at least 8 characters long")
+      if (data.confirmPassword === '') throw("Please confirm your password")
+      if (data.password !== data.confirmPassword) throw("Passwords do not match")
     }
     catch (error) {
       alert(error);
